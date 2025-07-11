6665f25301aee203fd5c9fc79fe416c80b9b586a
martinsalinas0 <martinlsalinas@pm.me>
2025-07-10 21:43:22 -0500

    Add Category type and update Jobs model to include location, completeBy, and category fields

--- src/models/jobs.models.ts
+++ src/models/jobs.models.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
-
+import { Category } from "../utils/types";
 //declares what fields JOBS  will have
 //IJOBS = interface of jobs
 interface IJobs extends Document {
@@ -9,6 +9,7 @@ interface IJobs extends Document {
   postedBy: "contractor" | "subcon";
   location: String;
   completeBy: String;
+  category: Category;
 }
 
 //the schema for mongoose
@@ -17,6 +18,13 @@ const JobsSchema: Schema<IJobs> = new Schema<IJobs>(
     jobName: { type: String, required: true, trim: true, lowercase: true },
     cost: { type: Number, required: true },
     postedBy: { type: String, enum: ["contractor", "subcon"], required: true },
+    location: { type: String, required: true },
+    completeBy: { type: String, required: true },
+    category: {
+      type: String,
+      enum: ["landscape", "plumbing", "electrician"],
+      required: true,
+    },
   },
   { timestamps: true }
 );
